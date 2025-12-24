dcb38f9b710aa3f01ff86142f0bbb4e9a43e66f9
kaushal karki <karkikaushal033@gmail.com>
2025-12-24 08:54:16 +0545

    Create NaturalNumber.java

---
+++ NaturalNumber.java
@@ -0,0 +1,11 @@
+//WAP to print 1-10 natural Number
+public class NaturalNumber {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+}
